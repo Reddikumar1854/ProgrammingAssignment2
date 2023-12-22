263248ca235d1cc166b4df8d784bb905f48626c6
REDDIKUMAR REDDY GALIGUTTA <galiguttareddi1854@gmail.com>
2023-12-22 20:04:11 +0530

    Completed makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,37 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <-NULL
+
+        set <- function(newvalue) {
+          x <<- newvalue
+          inv <<- NULL      
+        }
+
+        get <-function() {
+
+        }
 
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+    if (!is.null(inv)) {
+        message("Getting cache inverse")
+        return(inv)
+    } else {
+        message("Calculating inverse")
+        inv <- solve(x,...)
+        return(inv)
+    }   
+{
+
+     list(set = set, get = get, cacheSolve = cacheSolve)   
+}    
+
+cacheSolve <- function(cacheMatrix, ...) {
+  cacheMatrix$cacheSolve(...) 
         ## Return a matrix that is the inverse of 'x'
+}
 }
